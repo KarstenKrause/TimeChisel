e951ff279cc1900213170a9702efa3edf5cc530c
Karsten Krause <karsten.krause1988@gmail.com>
2024-02-01 23:57:40 +0100

    Initial Commit

---
+++ TimeChisel/TimeChiselApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeChiselApp.swift
+//  TimeChisel
+//
+//  Created by Karsten Krause on 01.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TimeChiselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
