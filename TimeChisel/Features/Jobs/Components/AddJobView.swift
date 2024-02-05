f73deb76834a6fa93c0b73719ba068cc38588b3b
Karsten Krause <karsten.krause1988@gmail.com>
2024-02-05 23:08:20 +0100

    added AddJobView

---
+++ TimeChisel/Features/Jobs/Components/AddJobView.swift
@@ -0,0 +1,18 @@
+//
+//  AddJobView.swift
+//  TimeChisel
+//
+//  Created by Karsten Krause on 04.02.24.
+//
+
+import SwiftUI
+
+struct AddJobView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddJobView()
+}
